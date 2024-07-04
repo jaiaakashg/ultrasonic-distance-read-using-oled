e5f160bd77d8bd0e18aa754c7dcd36387d3b363b
jaiaakashg <jaiaakashg28210@gmail.com>
2024-07-04 22:25:21 +0530

    Add files via upload

---
+++ utrasonic_distance_read_with_oled.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+void setup() {
+
+  Serial.begin(9600);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.print("Distance: ");
+  display.print(distance);
+  display.println(" cm");
+  display.display();
+
+  // Delay before the next reading
+  delay(500);
+}
+
